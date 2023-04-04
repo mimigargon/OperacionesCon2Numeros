4057b40869e1e3ee3b975f8bcebea58a90f8919a
Mimi <mireiag77@gmail>
2023-04-04 14:39:46 +0200

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OperacionesCon2Numeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 5;
+            int num2 = 4;
+
+            int suma = num1 + num2;
+            int resta = num1 - num2;
+            int multi = num1 * num2;
+            int divi = num1 / num2;
+            int resto = num1 % num2;
+
+            Console.WriteLine($"SUMA: {suma}");
+            Console.WriteLine($"RESTA: {resta}");
+            Console.WriteLine($"MULTIPLICACIÓN: {multi}");
+            Console.WriteLine($"DIVISIÓN: {divi}");
+            Console.WriteLine($"RESTO: {resto}"); 
+        }
+    }
+}
